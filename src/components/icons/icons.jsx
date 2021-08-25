93e87f874c6e136e1a91e9b9fbf229098f389745
Atharva Joshi <iamatharvaj@gmail.com>
2021-08-25 10:57:45 +0530

    [wip]: Collaborator [feat]: Note image

--- src/components/icons/icons.jsx
+++ src/components/icons/icons.jsx
@@ -98,7 +98,6 @@ export default function IconsGroup(props) {
     };
     Service.archiveNotes(noteData, token)
       .then((noteData) => {
-        console.log(noteData);
         props.displayNote();
       })
       .catch((error) => {
