e214c31315572de9ca8ac21d5f02a8314e2268b1
Atharva Joshi <iamatharvaj@gmail.com>
2021-09-21 10:57:45 +0530

    Refactored user interface

--- src/services/userService.js
+++ src/services/userService.js
@@ -8,15 +8,12 @@ export default class UserService {
     registration = (data) => {
         return axios.postMethod(`${this.baseURL}user/userSignUp`, data);
     }
-
     login = (data) => {
         return axios.postMethod(`${this.baseURL}user/login`, data);
     }
-
     forgetPassword = (data) => {
         return axios.postMethod(`${this.baseURL}user/reset`, data);
     }
-
     resetPassword = (data, token) => {
         return axios.postMethod(`${this.baseURL}user/reset-password`, data, { headers: {"Authorization" : token} });
     }
