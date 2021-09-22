81db6af3805849abe6405809eb961fb7dc5ec2f4
Atharva Joshi <iamatharvaj@gmail.com>
2021-09-22 10:57:45 +0530

    Refactored user interface

--- src/services/noteService.js
+++ src/services/noteService.js
@@ -3,7 +3,7 @@ import AxiosService from "./axiosService";
 const axios = new AxiosService();
 
 export default class NotesService {
-    baseURL = "http://fundoonotes.incubation.bridgelabz.com/api/";
+    baseURL = "http://fundoonotes.incubation.bridgelabz.com/api/ ";
 
     createNote = (data, token) => {
         return axios.postMethod(`${this.baseURL}notes/addNotes`, data, { headers: {"Authorization" : token} });
