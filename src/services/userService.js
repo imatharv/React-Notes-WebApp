fe34bf2655d5e81fbaca2b62c84950c067fc6b75
Atharva Joshi <iamatharvaj@gmail.com>
2021-09-22 10:57:45 +0530

    Refactored user interface

--- src/services/userService.js
+++ src/services/userService.js
@@ -6,18 +6,18 @@ export default class UserService {
     baseURL = "http://fundoonotes.incubation.bridgelabz.com/api/";
 
     registration = (data) => {
-        return axios.postMethod(`${this.baseURL}user/userSignUp/`, data);
+        return axios.postMethod(`${this.baseURL}user/userSignUp`, data);
     }
 
     login = (data) => {
-        return axios.postMethod(`${this.baseURL}user/login/`, data);
+        return axios.postMethod(`${this.baseURL}user/login`, data);
     }
 
     forgetPassword = (data) => {
-        return axios.postMethod(`${this.baseURL}user/reset/`, data);
+        return axios.postMethod(`${this.baseURL}user/reset`, data);
     }
 
     resetPassword = (data, token) => {
-        return axios.postMethod(`${this.baseURL}user/reset-password/`, data, { headers: {"Authorization" : token} });
+        return axios.postMethod(`${this.baseURL}user/reset-password`, data, { headers: {"Authorization" : token} });
     }
 }
